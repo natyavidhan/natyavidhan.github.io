3164d31074d4275765d0c7255aa44bba0ee6e19c
studiousgamer <natyavidhanbiswas10@gmail.com>
2022-08-01 23:57:10 +0530

    router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react'
-import './css/home.css';
+import { Routes, Route } from "react-router-dom"
+
 import Home from './components/Home';
+
+import './css/home.css';
 
 function App() {
   return (
     <div className='container center'>
-        <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   )
 }
