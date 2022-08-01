4cfcb7edae44c97718ebbe2de950b94bbb390778
studiousgamer <natyavidhanbiswas10@gmail.com>
2022-08-02 01:41:49 +0530

    skills page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom"
 
 import Home from './pages/Home';
 import About from './pages/About';
+import Skills from './pages/Skills';
 
 import './css/home.css';
 
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
+        <Route path="skills" element={<Skills />} />
       </Routes>
     </div>
   )
