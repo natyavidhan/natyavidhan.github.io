51b6872db32ef7bbd96869895d9358513a2c18fb
studiousgamer <natyavidhanbiswas10@gmail.com>
2022-12-10 00:30:24 +0530

    profile updated

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,10 +5,10 @@ function About() {
   return (
     <div id="about-me">
         <h2>About Me</h2>
-        <h3>I am a 15-year-old FullStack web, Software and Game developer from India. <br />
-            I use python as my main language, with that, I also have Experience with Javascript, a little bit of C# and C as well. <br />
-            I have made a few WebApps which are actually useful and can be used daily, I made few software which can also come handy. <br />
-            I worked on a few games, individually as well as in a team. I have experience with Godot, Pygame and Construct 2/3 for game development.
+        <h3>I am a 16-year-old FullStack web, software, and Game developer from Delhi, India. <br />
+I use python as my primary language, and with that, I have Experienced with Javascript, a little bit of C#, and C as well. <br />
+I have been developing for almost 2 years now, and I am still learning new things every day. <br />
+I Made lots of useful and functional software, web apps/websites, games, automation scripts, and much more during the span of these 2 years. <br />
         </h3>
         <Link to="/" className='back-btn'>Back to home</Link>
     </div>
